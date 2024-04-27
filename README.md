5053e468fbc10529ff230428022dfdcc0aef8081
Gorka Puente Díez <gorkapuentediez@gmail.com>
2024-04-27 11:48:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# CLISculpt
+2024 (c) gorkiiuss
+
+CLISculpt is an easy declarative framework for creating CLI Menu programs.
---
+++ src/main/java/com/github/gorkiiuss/clisculpt/CLISculpt.java
@@ -0,0 +1,15 @@
+package com.github.gorkiiuss.clisculpt;
+
+/**
+ * CLISculpt is an easy declarative framework for creating CLI Menu programs.
+ * @author gorkiiuss
+ * @version 1.0-SNAPSHOT
+ */
+public class CLISculpt {
+    /**
+     * Entry point of the framework
+     */
+    public static void main(String[] args) {
+
+    }
+}
